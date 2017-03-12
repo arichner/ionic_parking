9dd370cf3d777673f740b008f5f972fa9c58c42a
arichner <arichner@d.umn.edu>
2017-03-12 15:16:43 -0500

    BASIC google auth is working but everything else was discarded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,34 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbfdcslqsK58OWpHEwUDanyKglzXeJt14",
+  authDomain: "ionic-parking.firebaseapp.com",
+  databaseURL: "https://ionic-parking.firebaseio.com",
+  storageBucket: "ionic-parking.appspot.com",
+  messagingSenderId: "782451742975"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect,
+  scope: ['email','id','name','picture'],
+  offline: true
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
